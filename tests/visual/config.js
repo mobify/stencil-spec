d2febd33ccb6384064f0c2c5a3072a465799cf83
Ryan Frederick <ryan@ryanfrederick.com>
2015-05-26 16:52:45 -0700

    Get tests working again.

--- tests/visual/config.js
+++ tests/visual/config.js
@@ -1,6 +1,6 @@
 require.config({
     paths: {
-        'text': '../../bower_components/text/text',
+        'text-loader': '../../bower_components/text/text',
         'dust-full': '../../bower_components/dustjs-linkedin/dist/dust-full',
         'adaptivejs': '../../bower_components/adaptivejs',
     },
--- tests/visual/runner.js
+++ tests/visual/runner.js
@@ -1,19 +1,34 @@
 define(function(require) {
     var dust = require('dust-full');
     var componentHelper = require('adaptivejs/lib/dust-component-helper');
+    var sugar = require('adaptivejs/lib/dust-component-sugar');
+    var tests = require('text-loader!tests.dust');
+    var context;
+    var templates;
+    var compile = function(templates) {
+        dust = componentHelper(dust);
+        for (var helperName in templates) {
+            if (templates.hasOwnProperty(helperName)) {
+                dust.loadSource(dust.compile(templates[helperName], helperName));
+                dust.helpers[helperName] = sugar.makeHelper(helperName);
+            }
+        }
+    };
 
-    // Add the component helper to dust.
-    dust = componentHelper(dust);
-
-    // Register component helper and precompile the templates
-    // HOW???!!!
+    // Require in the component’s templates:
+    templates = {
+        'c-spec': require('text-loader!../../spec.dust'),
+        'c-spec__test': require('text-loader!../../spec__test.dust'),
+        'c-spec__case': require('text-loader!../../spec__case.dust')
+    };
 
     // Define any context required for the tests:
-    var context = {
+    context = {
         repo: 'https://github.com/mobify/stencil-spec'
     };
 
-    // Render
+    // Compile and render
+    compile(templates);
     dust.renderSource(tests, context, function(err, out) {
         if (!err) {
             document.querySelector('body').innerHTML = out;
